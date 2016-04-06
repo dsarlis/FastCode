eb146b416937b9edea16f3cc095d1691bcab112f
Gaurav Walia <email.gaurav.walia@gmail.com>
2016-04-05 20:15:35 -0700

    Code refactor by Gaurav and Dimitri

--- TermProject/src/main/java/sequential/util/FileIO.java
+++ TermProject/src/main/java/sequential/util/FileIO.java
@@ -1,8 +1,5 @@
 package sequential.util;
 
-import sequential.main.Graph;
-import sequential.main.Util;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,49 +9,60 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import sequential.model.Graph;
+
 public class FileIO {
 
-    public static Graph readGraph(InputStream inputStream) {
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
-            Map<Integer, List<Integer>> dynamicSizeGraph = new HashMap<Integer, List<Integer>>();
-            Map<String, Integer> mapping = new HashMap<String, Integer>();
-            String line = null;
-            int nodeCounter = 0;
+	public static Graph readGraph(InputStream inputStream) {
 
-            while ((line = bufferedReader.readLine()) != null) {
-                if (line.startsWith("#")) {
-                    continue;
-                }
-                String[] edge = line.split("\\s+");
-                String from = edge[0];
-                String to = edge[1];
+		Graph result = null;
 
-                if (!mapping.containsKey(from)) {
-                    mapping.put(from, nodeCounter++);
-                }
-                if (!mapping.containsKey(to)) {
-                    mapping.put(to, nodeCounter++);
-                }
-                List<Integer> neighbors = Util.getDefaultList(dynamicSizeGraph, mapping.get(from));
+		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-                neighbors.add(mapping.get(to));
-            }
-            List<Integer>[] graph  = new List[nodeCounter];
-            Map<Integer, String> invertedMapping = new HashMap<Integer, String>();
+			Map<Integer, List<Integer>> dynamicSizeGraph = new HashMap<>();
+			Map<String, Integer> mapping = new HashMap<>();
+			String line = null;
+			int nodeCounter = 0;
+			Map<Integer, String> invertedMapping = new HashMap<>();
 
-            graph = dynamicSizeGraph.values().toArray(graph);
-            for (int i = 0; i < nodeCounter; i++) {
-                if (graph[i] == null) {
-                    graph[i] = new ArrayList<Integer>();
-                }
-            }
-            for (Map.Entry<String, Integer> entry: mapping.entrySet()) {
-                invertedMapping.put(entry.getValue(), entry.getKey());
-            }
-            return new Graph(graph, invertedMapping);
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
-    }
+			while ((line = bufferedReader.readLine()) != null) {
+				if (line.startsWith("#")) {
+					continue;
+				}
+				String[] edge = line.split("\\s+");
+				String from = edge[0];
+				String to = edge[1];
+
+				if (!mapping.containsKey(from)) {
+					invertedMapping.put(nodeCounter, from);
+					mapping.put(from, nodeCounter++);
+				}
+				if (!mapping.containsKey(to)) {
+					invertedMapping.put(nodeCounter, to);
+					mapping.put(to, nodeCounter++);
+				}
+
+				List<Integer> neighbors = Util.getDefaultList(dynamicSizeGraph, mapping.get(from));
+				neighbors.add(mapping.get(to));
+			}
+
+			@SuppressWarnings("unchecked")
+			List<Integer>[] graph = new List[nodeCounter];
+
+			graph = dynamicSizeGraph.values().toArray(graph);
+			for (int i = 0; i < nodeCounter; i++) {
+				if (graph[i] == null) {
+					graph[i] = new ArrayList<Integer>();
+				}
+			}
+
+			result = new Graph(graph, invertedMapping);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
 
 }
